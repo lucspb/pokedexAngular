5c674d6d943e529f0428055ebdc693f6a523efdc
Lucas Estanislau <lucasestanislau@hotmail.com>
2022-12-11 08:22:21 -0300

    Criando funcoes para imagem

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./pokemon-card.component.sass']
 })
 export class PokemonCardComponent {
+
+  @Input() pokemon: string = '';
+
+  @Input() numero: number = 0;
+
+  getImagemPokemon(){
+    const numeroFormatado = this.addZero(this.numero);
+    
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${numeroFormatado}.png`
+  }
+
+  addZero(str: string | number, size = 3): string{
+    let s = String(str);
+
+    while(s.length < (size || 2)){
+      s = '0' + s;
+    }
+
+    return s;
+  }
 
 }
--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonListComponent implements OnInit {
 
+  pokemons = ['Bulbasaur', 'Ivysaur', 'Venosaur', 'Charmander'];
+
+
   constructor(){
   }
 
