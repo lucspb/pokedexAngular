b6c9229f5809c7d974447012d7b0a80d8c1a0c30
Lucas Estanislau <lucasestanislau@hotmail.com>
2022-12-11 07:29:26 -0300

    adicionando cards

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pokemon-list',
   templateUrl: './pokemon-list.component.html',
   styleUrls: ['./pokemon-list.component.sass']
 })
-export class PokemonListComponent {
+export class PokemonListComponent implements OnInit {
+
+  constructor(){
+  }
+
+  ngOnInit(): void{
+
+  }
 
 }
